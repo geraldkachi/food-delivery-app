60199413c5b87ba23f8d6476db233c3d5d6505cf
geraldkachi <fitzgeraldkachi@gmail.com>
2022-11-11 17:47:51 +0100

    latest

--- App.tsx
+++ App.tsx
@@ -1,20 +1,30 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 
 export default function App() {
+
+  const Stack = createStackNavigator();
+
+
   return (
-    <View style={styles.container}>
+    <NavigationContainer>
+    <View>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
